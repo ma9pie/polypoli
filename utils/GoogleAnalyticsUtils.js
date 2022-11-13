6de98f2f81766b2bae0dbf9b031a9cfbafcb9757
ma9pie <ma9pie.dev@gmail.com>
2022-11-13 17:26:42 +0900

    env 추가

--- utils/GoogleAnalyticsUtils.js
+++ utils/GoogleAnalyticsUtils.js
@@ -1,8 +1,7 @@
 const GoogleAnalyticsUtils = () => {};
 
-// 페이지 이동 감지
 GoogleAnalyticsUtils.changeRouteGtag = (url) => {
-  window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
+  window.gtag("config", process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
     page_path: url,
   });
 };
