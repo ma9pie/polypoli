74eeeabf96b94aae69476aff98ca9b742cc2498e
ma9pie <ma9pie.dev@gmail.com>
2022-11-13 17:55:56 +0900

    구글 애널리틱스 추적키 테스트

---
+++ pages/dataTest.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DataTest = () => {
+  return <div>DataTest</div>;
+};
+
+export default DataTest;
--- utils/GoogleAnalyticsUtils.js
+++ utils/GoogleAnalyticsUtils.js
@@ -1,6 +1,7 @@
 const GoogleAnalyticsUtils = () => {};
 
 GoogleAnalyticsUtils.changeRouteGtag = (url) => {
+  console.log(process.env.NEXT_PUBLIC_GA_TRACKING_ID);
   window.gtag("config", process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
     page_path: url,
   });
